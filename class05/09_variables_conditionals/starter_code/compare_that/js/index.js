f05aebe24d79c5ad13a248e10a8c051a6721cf27
ptrig1092 <Paul@Pauls-MacBook-Pro-2.local>
2016-03-01 19:10:23 -0500

    Update 3/1

---
+++ class05/09_variables_conditionals/starter_code/compare_that/js/index.js
@@ -0,0 +1,22 @@
+var comparison;
+
+function compare(){
+	var a = $("#a").val();
+	var b = $("#b").val();
+
+	if (a > b) {
+		comparison = '>';
+	} else if (a < b) {
+		comparison = '<';
+	} else if (a === b) {
+		comparison = '=';
+	} else {
+		comparison = 'N/A';
+	}
+
+	$("#comparison").html(comparison);
+}
+
+$(document).ready(function(){
+	$("#submit").click(compare);
+});
---
+++ class05/09_variables_conditionals/starter_code/score_keeper/js/index.js
@@ -0,0 +1,28 @@
+var total = 0;
+
+function zero() {
+	total = 0;
+	$("#result").html(total);
+};
+
+function add5() {
+	total = total +5;
+	$("#result").html(total);
+};
+
+function add10() {
+	total = total + 10;
+	$("#result").html(total);
+};
+
+function sub1() {
+	total = total - 1;
+	$("#result").html(total);
+};
+
+$(document).ready(function() {
+	$("#zero").click(zero);
+	$("#add5").click(add5);
+	$("#add10").click(add10);
+	$("#sub1").click(sub1);
+});
